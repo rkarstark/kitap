e53b9facd63e64ab6cf2e4c2bae655bc79e670b5
Çağrı Gökmen <57169827+rkarstark@users.noreply.github.com>
2022-05-02 16:57:13 +0300

    Update settings.py

--- kitap_pazari/kitap_pazari/settings.py
+++ kitap_pazari/kitap_pazari/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%%y@pvoiermw1y4c&+r_#j_bnx$_j!0z5gi($z-!zqzc^6pjy5'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
